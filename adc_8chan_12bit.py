8ae9dfcfdc7785baf8b14b037b6da7cf15ce729d
Daniel Boyd <belteshazzarboyd@gmail.com>
2025-04-09 15:54:06 -0400

    Updated adc_8chan_12bit.py to use new firmware

--- adc_8chan_12bit.py
+++ adc_8chan_12bit.py
@@ -8,6 +8,7 @@ REG_RAW_DATA_START = 0x10
 REG_VOL_START = 0x20
 REG_RTO_START = 0x30
 REG_SET_ADDR = 0xC0
+COMMAND_CHANGE_ADDR = 0x55
 
 
 class Pi_hat_adc():
@@ -16,17 +17,21 @@ class Pi_hat_adc():
         self.addr = addr
 
     def set_i2c_address(self, new_addr):
-        """Change the I2C address of the ADC"""
+        """Change the I2C address of the ADC (new firmware requires command byte 0x55)"""
+        if new_addr < 0x08 or new_addr > 0x77:
+            return False
+
         try:
-            self.bus.write_i2c_block_data(self.addr, REG_SET_ADDR, [new_addr])
+            # New firmware expects [0x55, new_addr] at register 0xC0
+            self.bus.write_i2c_block_data(self.addr, REG_SET_ADDR, [COMMAND_CHANGE_ADDR, new_addr])
+            time.sleep(0.1)  # Allow time for flash write and reset
             self.addr = new_addr  # Update local address
-            time.sleep(0.1)  # Allow time for address change
             return True
         except:
             return False
 
     def get_all_adc_raw_data(self):
-        """Get all 8 channels raw data (12-bit)"""
+        """Get all 8 channels raw data (12-bit) - same as before"""
         array = []
         for i in range(ADC_CHAN_NUM):
             data = self.bus.read_i2c_block_data(self.addr, REG_RAW_DATA_START + i, 2)
@@ -36,6 +41,8 @@ class Pi_hat_adc():
 
     def get_nchan_adc_raw_data(self, n):
         """Get single channel raw data"""
+        if n < 0 or n >= ADC_CHAN_NUM:
+            return 0
         data = self.bus.read_i2c_block_data(self.addr, REG_RAW_DATA_START + n, 2)
         return data[1] << 8 | data[0]
 
@@ -50,6 +57,8 @@ class Pi_hat_adc():
 
     def get_nchan_vol_milli_data(self, n):
         """Get single channel voltage"""
+        if n < 0 or n >= ADC_CHAN_NUM:
+            return 0
         data = self.bus.read_i2c_block_data(self.addr, REG_VOL_START + n, 2)
         return data[1] << 8 | data[0]
 
@@ -64,6 +73,8 @@ class Pi_hat_adc():
 
     def get_nchan_ratio_0_1_data(self, n):
         """Get single channel ratio"""
+        if n < 0 or n >= ADC_CHAN_NUM:
+            return 0
         data = self.bus.read_i2c_block_data(self.addr, REG_RTO_START + n, 2)
         return data[1] << 8 | data[0]
 
